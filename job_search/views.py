e3a883a88c9e2b43a5956e0675203c7dd7c7a33b
Nikolay <k0jl9ih@yandex.ru>
2021-07-17 13:31:02 +0300

    week 3 done

--- job_search/views.py
+++ job_search/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import get_object_or_404
 # Create your views here.
 from django.views.generic import TemplateView, ListView
@@ -31,11 +31,25 @@ class VacanciesView(ListView):
     model = Vacancy
     context_object_name = 'vacancies'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # Тайтл в темплэйте. Если все вакансии - "все вакансии", или по категориям
+        context = super(VacanciesView, self).get_context_data(**kwargs)
+        if self.kwargs:
+            try:
+                category = Speciality.objects.get(code=self.kwargs['category'])
+                context['title'] = category.title
+            except Speciality.DoesNotExist:
+                raise Http404
+        else:
+            context['title'] = "Все вакансии"
+        print(context)
+        return context
+
     def get_queryset(self):
 
         if self.kwargs:  # если урл что-то передает
             category = self.kwargs['category']
-            if Speciality.objects.filter(code=category).count() > 0:
+            if Speciality.objects.filter(code=category).count() > 0:  # наличие специализации из урла в списке
                 return Vacancy.objects.filter(specialty__code=category)   # поиск по свециализации
             else:
                 raise Http404
@@ -51,3 +65,10 @@ class CompanyView(TemplateView):
         context['company'] = get_object_or_404(Company, pk=context['company_pk'])  # объект по ключу из url
         context['companies_count'] = Company.objects.count()  # передается для перехода на следующую вакансию
         return context
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ой, что то сломалось... Простите извините! (Ошибка 404)')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Ой, что то сломалось... Простите извините! (Ошибка 500)')
